d80debcc80efb69abeaa2ca9efe259dd0f1a2ac8
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-03-03 01:16:05 +0530

    Layout

--- nextjsproject/app/[username]/community/page.tsx
+++ nextjsproject/app/[username]/community/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function communityPage({params} : {params : {username : string}}) {
-    const {username} = params;
-  return (
-    <div>
-      Set Community User Name {username}
-    </div>
-  )
+function communityPage({ params }: { params: { username: string } }) {
+    const { username } = params;
+    return (
+        <div>
+            Set Community User Name {username}
+        </div>
+    )
 }
 
 export default communityPage
